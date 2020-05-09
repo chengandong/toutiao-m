77abee34b4c8fe9b6a59106cd176753c4eb52d29
FreeLife <380398082@qq.com>
2020-05-09 12:44:36 +0800

    初始化项目目录结构 以及导入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入 Vant 组件库
+import Vant from 'vant'
+// 导入 Vant 组件库 样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
