792ba2a3b80f6f307bd9406132d6da443db29536
FreeLife <380398082@qq.com>
2020-05-09 19:08:58 +0800

    配置路由页面-(登录,首页,问答,视频,我的)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <template>
   <div id="app">
-    <h1>头条</h1>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
-    <i class="toutiao toutiao-shipin"></i>
-    <i class="toutiao toutiao-wuwangluo"></i>
-    <van-button icon-prefix="toutiao" icon="wode" type="primary">按钮</van-button>
     <!-- 路由出口 -->
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-.box {
-  width: 200px;
-  height: 200px;
-  background-color: pink;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,39 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+// 配置 路由规则
 const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login/')
+  },
+  {
+    path: '/',
+    component: () => import('@/views/layout/'),
+    children: [
+      {
+        path: '', // path 为空，会默认 指向子路由
+        name: 'home',
+        component: () => import('@/views/home/')
+      },
+      {
+        path: '/qa',
+        name: 'qa',
+        component: () => import('@/views/qa/')
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: () => import('@/views/video/')
+      },
+      {
+        path: '/my',
+        name: 'my',
+        component: () => import('@/views/my/')
+      }
+    ]
+  }
 ]
 
 const router = new VueRouter({
