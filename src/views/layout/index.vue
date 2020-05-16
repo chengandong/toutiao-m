5dd45d58a9746fa52da7429385e79debd2af02f0
FreeLife <380398082@qq.com>
2020-05-16 11:36:09 +0800

    页面底部导航栏-图标高亮显示处理 以及登录与未登录文字显示状态

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -5,31 +5,69 @@
     <!-- 底部导航栏 -->
     <van-tabbar v-model="active" route>
       <van-tabbar-item
-        icon="home-o"
+        :icon="ishomeShow ? 'wap-home' : 'home-o'"
         to="/"
+        @click="iconHome"
       >首页</van-tabbar-item>
       <van-tabbar-item
-        icon="comment-o"
+        :icon="iscommentShow ? 'comment' : 'comment-o'"
         to="/qa"
+        @click="iconComment"
         >问答</van-tabbar-item>
       <van-tabbar-item
-        icon="video-o"
+        :icon="isvideoShow ? 'video' : 'video-o'"
         to="/video"
+        @click="iconVideo"
       >视频</van-tabbar-item>
       <van-tabbar-item
-        icon="manager-o"
+        :icon="ismanagerShow ? 'manager' : 'manager-o'"
         to="/my"
-      >我的</van-tabbar-item>
+        @click="iconManager"
+      >{{user ? '我的' : '未登录'}}</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'LayoutIndex',
   data () {
     return {
-      active: 0
+      active: 0,
+      ishomeShow: false,
+      iscommentShow: false,
+      isvideoShow: false,
+      ismanagerShow: false
+    }
+  },
+  computed: {
+    ...mapState(['user'])
+  },
+  methods: {
+    iconHome () {
+      this.ishomeShow = true
+      this.iscommentShow = false
+      this.isvideoShow = false
+      this.ismanagerShow = false
+    },
+    iconComment () {
+      this.ishomeShow = false
+      this.iscommentShow = true
+      this.isvideoShow = false
+      this.ismanagerShow = false
+    },
+    iconVideo () {
+      this.ishomeShow = false
+      this.iscommentShow = false
+      this.isvideoShow = true
+      this.ismanagerShow = false
+    },
+    iconManager () {
+      this.ishomeShow = false
+      this.iscommentShow = false
+      this.isvideoShow = false
+      this.ismanagerShow = true
     }
   }
 }
