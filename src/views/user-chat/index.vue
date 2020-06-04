dc7260495d583cc0f65a18a37c634eda06ced6cb
FreeLife <380398082@qq.com>
2020-06-04 22:40:15 +0800

    小程同学-聊天功能基本实现(动态渲染样式还有问题)

--- src/views/user-chat/index.vue
+++ src/views/user-chat/index.vue
@@ -8,27 +8,23 @@
       @click-left="$router.back()"
     />
     <!-- 消息列表 -->
-    <van-cell-group class="messageList">
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
-      <van-cell title="单元格" />
+    <van-cell-group class="messageList" ref="message-list">
+      <van-cell
+        class="message-item"
+        :border="false"
+        v-for="(message, index) in messageList"
+        :key="index"
+      >
+        <van-image
+          :slot="index % 2 === 0 ? 'default' : 'title'"
+          width="40"
+          height="40"
+          :src=" index % 2 === 0 ? 'http://toutiao.meiduo.site/FqHn7ps9v5I8esWXJNKH0asrSwcB' : 'http://toutiao.meiduo.site/FlOAkWHoU8lnYwU6eCEPN-dHINHl'"
+        />
+        <div class="msg-wrap" :slot="index % 2 === 0 ? 'title' : 'default'">
+          <span class="title">{{message.msg}}</span>
+        </div>
+      </van-cell>
     </van-cell-group>
     <!-- 发送消息 -->
     <van-cell-group class="sendMessage-wrap">
@@ -37,30 +33,102 @@
         placeholder="请输入消息"
         :border="false"
       />
-      <van-button type="primary" size="small">发送</van-button>
+      <van-button type="primary" size="small" @click="onSend">发送</van-button>
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client'
+import { getItem, setItem } from '@/utils/storage'
 export default {
   name: 'UserChat',
   data () {
     return {
-      message: ''
+      message: '', // 输入框中信息
+      socket: null, // WebSocket 通信对象
+      messageList: getItem('chat-messages') || [] // 消息列表数据
+    }
+  },
+  watch: {
+    messageList () {
+      setItem('chat-messages', this.messageList)
+      // 数据改变之后 立即操作 DOM
+      this.$nextTick(() => {
+        this.scrollToBottom()
+      })
+    }
+  },
+  created () {
+    // 建立连接
+    const socket = io('http://ttapi.research.itcast.cn')
+
+    this.socket = socket
+
+    socket.on('connect', () => {
+      console.log('连接建立成功了')
+    })
+
+    // 监听message事件, 监听从服务器返回的数据
+    socket.on('message', data => {
+      this.messageList.push(data)
+    })
+
+    // 断开连接
+    socket.on('disconnect', () => {
+      console.log('连接断开')
+    })
+  },
+  mounted () {
+    this.scrollToBottom()
+  },
+  methods: {
+    // 发送消息
+    onSend () {
+      // 后台需要的参数
+      const data = {
+        msg: this.message,
+        timestamp: Date.now()
+      }
+      this.socket.emit('message', data)
+
+      // 将用户 发出的数据 存入数组
+      this.messageList.push(data)
+
+      // 清空输入框
+      this.message = ''
+    },
+    scrollToBottom () {
+      const list = this.$refs['message-list']
+      list.scrollTop = list.scrollHeight
     }
   }
 }
 </script>
 
 <style scoped lang="less">
-.messageList {
+/deep/ .messageList {
   position: fixed;
   left: 0;
   right: 0;
   top: 46px;
   bottom: 44px;
   overflow-y: auto;
+  background-color: #f5f5f6;
+  .van-cell {
+    background-color: #f5f5f6;
+  }
+}
+.message-item {
+  display: flex;
+  align-items: center;
+  .msg-wrap {
+    flex: 1;
+    .title {
+      background: #e0effb;
+      padding: 10px;
+    }
+  }
 }
 .sendMessage-wrap {
   position: fixed;
